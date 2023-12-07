4bddc08d9b119c34e3fc737b68382e725e9617a1
@Deng <dengelijah30@gmail.com>
2023-12-07 12:56:01 +0300

    initial commit

---
+++ main.js
@@ -0,0 +1,11 @@
+function print(){
+  var x= document.getElementById("floatingInput").value;
+  var y= document.getElementById("inputPassword5").value;
+  var link=document.createElement("a");
+
+  
+  alert("welcome to My profile")
+
+
+
+}
